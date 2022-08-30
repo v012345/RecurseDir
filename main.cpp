2957c9dbd822cf52e967eaf021e090818a8005a9
v138468 <v138468@gmail.com>
2022-08-30 13:06:36 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main()
+{
+    // fs::current_path(fs::temp_directory_path());
+    // fs::create_directories("sandbox/a/b");
+    // std::ofstream("sandbox/file1.txt");
+    // fs::create_symlink("a", "sandbox/syma");
+
+    // // Iterate over the `std::filesystem::directory_entry` elements explicitly
+    // for (const fs::directory_entry &dir_entry :
+    //      fs::recursive_directory_iterator("sandbox"))
+    // {
+    //     std::cout << dir_entry << '\n';
+    // }
+    // std::cout << "-----------------------------\n";
+    // // Iterate over the `std::filesystem::directory_entry` elements using `auto`
+    // for (auto const &dir_entry : fs::recursive_directory_iterator("sandbox"))
+    // {
+    //     std::cout << dir_entry << '\n';
+    // }
+
+    // fs::remove_all("sandbox");
+}
